3be0d923813787bfa2a504f2666b76e5655a8e97
chaos <i@geeku.org>
2012-08-22 16:07:19 +0800

    Create Project - TabHostDemo Signed-off-by: chaos <i@geeku.org>

---
+++ src/org/geeku/tabhostdemo/MainActivity.java
@@ -0,0 +1,100 @@
+package org.geeku.tabhostdemo;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.TabActivity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
+import android.widget.TabHost;
+import android.support.v4.app.NavUtils;
+
+public class MainActivity extends TabActivity implements
+		OnCheckedChangeListener {
+
+	private TabHost tabhost;
+	private RadioGroup mainTab;
+	private Intent iHome;
+	private Intent iNews;
+	private Intent iInfo;
+	private Intent iSearch;
+	private Intent iMore;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.activity_main);
+		mainTab = (RadioGroup) this.findViewById(R.id.main_tab);
+		mainTab.setOnCheckedChangeListener(this);
+		tabhost = getTabHost();
+		
+		iHome = new Intent(this, HomeActivity.class);
+		tabhost.addTab(tabhost
+				.newTabSpec("iHome")
+				.setIndicator(getResources().getString(R.string.main_home),
+						getResources().getDrawable(R.drawable.icon_1_n))
+				.setContent(iHome));
+		iNews = new Intent(this, NewsActivity.class);
+		tabhost.addTab(tabhost
+				.newTabSpec("iNews")
+				.setIndicator(getResources().getString(R.string.main_news),
+						getResources().getDrawable(R.drawable.icon_2_n))
+				.setContent(iNews));
+		iInfo=new Intent(this,MyInfoActivity.class);
+		tabhost.addTab(tabhost
+				.newTabSpec("iInfo")
+				.setIndicator(getResources().getString(R.string.main_my_info),
+						getResources().getDrawable(R.drawable.icon_3_n))
+				.setContent(iInfo));
+		iSearch = new Intent(this, SearchActivity.class);
+		tabhost.addTab(tabhost
+				.newTabSpec("iSearch")
+				.setIndicator(getResources().getString(R.string.menu_search),
+						getResources().getDrawable(R.drawable.icon_4_n))
+				.setContent(iSearch));
+		iMore = new Intent(this, MoreActivity.class);
+		tabhost.addTab(tabhost
+				.newTabSpec("iMore")
+				.setIndicator(getResources().getString(R.string.menu_settings),
+						getResources().getDrawable(R.drawable.icon_5_n))
+				.setContent(iMore));
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+	@Override
+	public void onCheckedChanged(RadioGroup group, int checkedId) {
+		// TODO Auto-generated method stub
+		switch (checkedId) {
+		case R.id.radio_button0:
+			this.tabhost.setCurrentTabByTag("iHome");
+			break;
+		case R.id.radio_button1:
+			this.tabhost.setCurrentTabByTag("iNews");
+			break;
+		case R.id.radio_button2:
+			this.tabhost.setCurrentTabByTag("iInfo");
+			break;
+		case R.id.radio_button3:
+			this.tabhost.setCurrentTabByTag("iSearch");
+			break;
+		case R.id.radio_button4:
+			this.tabhost.setCurrentTabByTag("iMore");
+			break;
+
+		default:
+			break;
+		}
+
+	}
+
+}
---
+++ src/org/geeku/tabhostdemo/SearchActivity.java
@@ -0,0 +1,17 @@
+package org.geeku.tabhostdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SearchActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		TextView textView=new TextView(this);
+		textView.setText("这是搜索！");
+		setContentView(textView);
+	}
+	
+}
